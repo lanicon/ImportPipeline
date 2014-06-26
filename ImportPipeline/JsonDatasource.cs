aad630668036ac8066dadd72b147710eb7b5a772
pweerd <pw1@bitmanager.nl>
2014-06-26 08:59:41 +0200

    new TestDatasource to emit values for most general known types new (premature) JsonDatasource

---
+++ ImportPipeline/JsonDatasource.cs
@@ -0,0 +1,116 @@
+﻿using Bitmanager.ImportPipeline;
+using Bitmanager.Core;
+using Bitmanager.Json;
+using Newtonsoft.Json.Linq;
+
+using Bitmanager.Xml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Xml;
+using System.Net;
+using System.IO;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Threading;
+using Bitmanager.Elastic;
+using Newtonsoft.Json;
+
+namespace Bitmanager.ImportPipeline
+{
+   public class JsonDatasource : Datasource
+   {
+      private IDatasourceFeeder feeder;
+      private bool dumpReader;
+      public void Init(PipelineContext ctx, XmlNode node)
+      {
+         feeder = ctx.CreateFeeder(node);
+         dumpReader = node.OptReadBool("@debug", false);
+      }
+
+
+      private StringDict getAttributes(XmlNode node)
+      {
+         StringDict ret = new StringDict();
+         if (node == null) return ret;
+         var coll = node.Attributes;
+         for (int i = 0; i < coll.Count; i++)
+         {
+            var att = coll[i];
+            if (att.LocalName.Equals("url", StringComparison.InvariantCultureIgnoreCase)) continue;
+            if (att.LocalName.Equals("baseurl", StringComparison.InvariantCultureIgnoreCase)) continue;
+            ret[att.LocalName] = att.Value;
+         }
+         return ret;
+      }
+
+      private static ExistState toExistState(Object result)
+      {
+         if (result == null || !(result is ExistState)) return ExistState.NotExist;
+         return (ExistState)result;
+      }
+      private void importUrl(PipelineContext ctx, IDatasourceSink sink, IDatasourceFeederElement elt)
+      {
+         StringDict attribs = getAttributes(elt.Context);
+         var fullElt = (FileNameFeederElement)elt;
+         String fileName = fullElt.FileName;
+         sink.HandleValue(ctx, "_start", fileName);
+         DateTime dtFile = File.GetLastWriteTimeUtc(fileName);
+         sink.HandleValue(ctx, "record/lastmodutc", dtFile);
+         sink.HandleValue(ctx, "record/virtualFilename", fullElt.VirtualFileName);
+
+         ExistState existState = ExistState.NotExist;
+         if ((ctx.ImportFlags & _ImportFlags.ImportFull) == 0) //Not a full import
+         {
+            existState = toExistState(sink.HandleValue(ctx, "record/_checkexist", null));
+         }
+
+         //Check if we need to convert this file
+         if ((existState & (ExistState.ExistSame | ExistState.ExistNewer | ExistState.Exist)) != 0)
+         {
+            ctx.Skipped++;
+            ctx.ImportLog.Log("Skipped: {0}. Date={1}", fullElt.VirtualFileName, dtFile);
+            return;
+         }
+
+         List<String> keys = new List<string>();
+         List<String> values = new List<String>();
+         int lvl = -1;
+         FileStream fs = null;
+         try
+         {
+            fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 16 * 1024);
+            JsonTextReader rdr = new JsonTextReader  (new StreamReader (fs, true));
+            JObject obj = (JObject)JObject.ReadFrom(rdr);
+            rdr.Close();
+            fs.Close();
+
+            Pipeline.EmitToken (ctx, sink, obj, "record", -1);
+         }
+         catch (Exception e)
+         {
+            if (!sink.HandleException(ctx, "record", e))
+               throw;
+         }
+      }
+
+      public void Import(PipelineContext ctx, IDatasourceSink sink)
+      {
+         foreach (var elt in feeder)
+         {
+            try
+            {
+               importUrl(ctx, sink, elt);
+            }
+            catch (Exception e)
+            {
+               throw new BMException(e, e.Message + "\r\nUrl=" + elt.Element + ".");
+            }
+         }
+      }
+
+   }
+}
---
+++ ImportPipeline/TestDatasource.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bitmanager.Core;
+using Bitmanager.Xml;
+using Bitmanager.Elastic;
+using Bitmanager.Json;
+using Newtonsoft.Json.Linq;
+using System.Xml;
+using System.Net;
+
+namespace Bitmanager.ImportPipeline
+{
+   public class TestDatasource : Datasource
+   {
+      public void Init(PipelineContext ctx, XmlNode node)
+      {
+      }
+
+      public void Import(PipelineContext ctx, IDatasourceSink sink)
+      {
+         sink.HandleValue(ctx, "record/double", 123.45);
+         sink.HandleValue(ctx, "record/date", DateTime.Now);
+         sink.HandleValue(ctx, "record/utcdate", DateTime.UtcNow);
+         sink.HandleValue(ctx, "record/int", -123);
+         sink.HandleValue(ctx, "record/string", "foo bar");
+         sink.HandleValue(ctx, "record", null);
+      }
+   }
+}

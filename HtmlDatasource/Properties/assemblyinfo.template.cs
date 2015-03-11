d25ed2217377c6ecf2f3184595f09a9f37658af6
pweerd <pw1@bitmanager.nl>
2015-03-11 22:24:25 +0100

    new build procedure

--- HtmlDatasource/Properties/assemblyinfo.template.cs
+++ HtmlDatasource/Properties/assemblyinfo.template.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
+//revno=116
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HtmlDatasource (SHA:$$GIT$$)")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("$$id$$ (SHA:$$GIT$$)")]
+[assembly: AssemblyDescription("$$DESCRIPTION$$")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("$$COMPANY$$")]
 [assembly: AssemblyProduct("HtmlDatasource")]
-[assembly: AssemblyCopyright("Copyright © $$YYYY$$, $$COMPANY$$")]
+[assembly: AssemblyCopyright("$$COPYRIGHT$$")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.$$REVNO$$")]
-[assembly: AssemblyFileVersion("1.0.0.$$REVNO$$")]
+[assembly: AssemblyVersion("$$VERSION$$")]
+[assembly: AssemblyFileVersion("$$VERSION$$")]

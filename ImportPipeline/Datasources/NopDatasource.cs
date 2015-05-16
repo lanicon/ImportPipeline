d6cd65cd400951cf8d24feabfd0192b1758e1916
pweerd <pw1@bitmanager.nl>
2015-05-16 13:53:44 +0200

    NOP datasource introduced

---
+++ ImportPipeline/Datasources/NopDatasource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Bitmanager.Core;
+using Bitmanager.Xml;
+using System.Data.SqlClient;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+using System.Data.Odbc;
+
+namespace Bitmanager.ImportPipeline
+{
+   public class NopDatasource : Datasource
+   {
+      public void Init(PipelineContext ctx, XmlNode node)
+      {
+      }
+
+      public void Import(PipelineContext ctx, IDatasourceSink sink)
+      {
+      }
+   }
+}

a3eded100a193bc1d7e728f5ed478fb3b96e490e
pweerd <pw1@bitmanager.nl>
2016-08-08 16:41:09 +0200

    TikaService is now a separate repo.

--- TikaService/browser.bat
+++
@@ -1,1 +0,0 @@
-start chrome "http://localhost:8080/html/filesystem/C:/Users/pweerd/Documents/oude root E/Snowy.eml"
--- TikaService/srv.bat
+++
@@ -1,2 +0,0 @@
-C:\Java\jdk1.7.0_25\bin\java  -jar "jetty-runner-8.1.15.v20140411.jar"  "target\tikaservice-1.0.war"
-pause

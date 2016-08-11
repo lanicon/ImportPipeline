9ba19327ed2adf249bc7925dc5151935b1f1d40b
pweerd <pw1@bitmanager.nl>
2016-08-11 17:39:06 +0200

    New object: TimeBasedIdGenerator, which mimics the id generation in elasticsearch. (primarily used for generating the same ID for meta/detail records)

---
+++ ImportPipeline/TimeBasedIdGenerator.cs
@@ -0,0 +1,62 @@
+﻿using Bitmanager.Core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bitmanager.ImportPipeline
+{
+   public class TimeBasedIdGenerator
+   {
+      static readonly long ticks2000 = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc).Ticks;
+
+      readonly Object _lock;
+      long last_ticks;
+      uint seq;
+
+
+      public TimeBasedIdGenerator()
+      {
+         _lock = new Object();
+      }
+
+      public long GetNextId()
+      {
+         long ticks = Math.Max(last_ticks, (DateTime.UtcNow.Ticks - ticks2000) / 10000);
+         lock (_lock)
+         {
+            seq = ((1+seq) &0xFFFFFF);
+            if (seq==0) ticks++;
+
+            last_ticks = ticks;
+         }
+
+         return ((ticks << 24)) | seq;
+      }
+
+      /// <summary>
+      /// Return the ID in Big endian format. This makes it possible that the String-sort on ID equals the insertion order
+      /// </summary>
+      public byte[] GetNextIdAsBytes()
+      {
+         long id = GetNextId();
+         byte[] ret = new byte[8];
+         int shift = 64 - 8;
+         for (int i = 0; i < ret.Length; i++, shift-=8)
+            ret[i] = (byte)(id >> shift);
+         return ret;
+      }
+
+      /// <summary>
+      /// Return the ID in Big endian Base64 format. This makes it possible that the String-sort on ID equals the insertion order
+      /// </summary>
+      public String GetNextIdAsString()
+      {
+         byte[] b = GetNextIdAsBytes();
+         return Base64Codec.UrlFriendly.Encode (b, 0, b.Length);
+      }
+   }
+}
---
+++ ImportPipeline/UnitTests/TimeBasedIdGeneratorTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bitmanager.ImportPipeline;
+using System.Threading;
+
+namespace UnitTests
+{
+   [TestClass]
+   public class TimeBasedIdGeneratorTest
+   {
+      [TestMethod]
+      public void TestMethod1()
+      {
+         var gen = new TimeBasedIdGenerator();
+
+         long id0 = gen.GetNextId();
+         Thread.Sleep(1000);
+         long id1 = gen.GetNextId();
+
+         Assert.AreEqual(1, (id1 & 0xFFFFFF) - (id0 & 0xFFFFFF));
+
+         long diff = (id1>>24) - (id0>>24);
+         Console.WriteLine("IDs={0:X} , {1:X}, XOR={2:X}", id0, id1, 0);
+         Console.WriteLine("diff=" + diff);
+         Assert.AreEqual(1000.0, diff, 10.0);
+
+         String s1 = gen.GetNextIdAsString();
+         String s2 = gen.GetNextIdAsString();
+         Console.WriteLine("S1=" + s1);
+         Console.WriteLine("S2=" + s2);
+         Assert.AreNotEqual(s1, s2);
+      }
+   }
+}
